df83d6bc43bc2f36358d87e2a551a8acdf0cc72b
Jake Harris <jakeharris30@outlook.com>
2023-07-06 15:23:30 +0100

    corrected file names

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ app.get('/temp/icon', (req, res) => {
 });
 
 app.get('/loader', (req, res) => {
-  res.download('./Build/unity-gamee.loader.js');
+  res.download('./Build/unity-gameeee.loader.js');
 });
 
 app.get('/data', (req, res) => {
-  res.download('./Build/unity-gameeee.loader.js');
+  res.download('./Build/unity-gameeee.data.br');
 });
 
 app.get('/framework', (req, res) => {
