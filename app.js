2ccec21860ea2c0733ca895848fe51a5d762f9d7
Jake Harris <jakeharris30@outlook.com>
2023-07-06 15:20:14 +0100

    unity utilised

--- app.js
+++ app.js
@@ -2,8 +2,28 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.download('./duck.jpg');
+app.get('/temp/styles', (req, res) => {
+  res.download('./TemplateData/style.css');
+});
+
+app.get('/temp/icon', (req, res) => {
+  res.download('./TemplateData/favicon.ico');
+});
+
+app.get('/loader', (req, res) => {
+  res.download('./Build/unity-gamee.loader.js');
+});
+
+app.get('/data', (req, res) => {
+  res.download('./Build/unity-gameeee.loader.js');
+});
+
+app.get('/framework', (req, res) => {
+  res.download('./Build/unity-gameeee.framework.js.br');
+});
+
+app.get('/wasm', (req, res) => {
+  res.download('./Build/unity-gameeee.wasm.br');
 });
 
 app.listen(3000, console.log('Connected'));
